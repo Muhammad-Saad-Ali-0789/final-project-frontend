1eeae9bccbfa911385d4fe78ca419e701c9bc8c6
Saadali198 <muhammadsaadakbar.386@gmail.com>
2026-01-22 15:57:31 +0500

    9

--- vite-project/src/context/Authcontext.jsx
+++ vite-project/src/context/Authcontext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL || 'https://final-project-backend-6isk.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
