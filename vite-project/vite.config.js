eccb2d99d36ae1fb272ebabc502af60bc95951ac
Saadali198 <muhammadsaadakbar.386@gmail.com>
2026-01-22 12:45:11 +0500

    backend url replace

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        target: process.env.VITE_API_URL || 'https://final-project-backend-6isk.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
